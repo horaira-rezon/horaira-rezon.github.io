ab5245b1fed62a2d90b5d4ebe349d558e62e5081
Horaira Rezon <40006280+allsabaab@users.noreply.github.com>
2025-06-13 17:24:36 +0600

    script.js

--- script.js
+++ script.js
@@ -157,3 +157,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+  this.classList.toggle('active');
+  document.querySelector('.nav-links').classList.toggle('show');
+});
